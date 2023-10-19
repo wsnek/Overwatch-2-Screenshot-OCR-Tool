364894b863bdfa97b769d332bd540537b042e019
wsnekk <wsnekk@gmail.com>
2023-10-18 21:44:28 -0600

    Made a semi-functional program that is capable of starting screenshot.exe, needs work, search "TODO" in MainWindow.xaml.cs to see integrated todo list for now. #11

--- UI/UI/MainWindow.xaml.cs
+++ UI/UI/MainWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -23,6 +21,48 @@ namespace UI
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Add your button click logic here
+            MessageBox.Show("Button clicked!");
+        }
+
+        private void StartScreenshotTool_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Process screenshotProcess = new Process();
+                screenshotProcess.StartInfo.FileName = "PATH_TO_SCREENSHOT_EXE_HERE"; // Replace with the path to your ScreenshotTool.exe
+                // TODO: update the find screenshot.exe process to use screenshot.exe that is present in the same file as UI.exe, to prevent strange things from happening
+                // TODO: program will freeze up when the "Start screenshot Tool" picture is taken
+                // TODO: program will continue taking screenshots after the program itself is closed, closing the program should completely destroy all processes.
+                // TODO: when minimizing the application, make it go to the system tray
+                // TODO: add icon
+                screenshotProcess.StartInfo.UseShellExecute = false;
+                screenshotProcess.StartInfo.CreateNoWindow = true; // This will prevent the console window from opening
+                screenshotProcess.StartInfo.RedirectStandardOutput = true;
+
+                screenshotProcess.OutputDataReceived += new DataReceivedEventHandler((s, args) =>
+                {
+                    outputTextBox.Dispatcher.Invoke(() =>
+                    {
+                        outputTextBox.Text += args.Data + Environment.NewLine;
+                    });
+                });
+
+                screenshotProcess.Start();
+                screenshotProcess.BeginOutputReadLine(); // Start asynchronous read operations on the redirected StandardOutput stream
+
+                screenshotProcess.WaitForExit();
+                screenshotProcess.Close();
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions here
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
         }
     }
 }
