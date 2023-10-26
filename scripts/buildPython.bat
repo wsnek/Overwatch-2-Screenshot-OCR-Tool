76607078a467dba8443cce9d8b77121ac322dd25
wsnekk <wsnekk@gmail.com>
2023-10-26 11:37:18 -0600

    Updated batch file for file structure purposes.

--- scripts/buildPython.bat
+++ scripts/buildPython.bat
@@ -2,15 +2,17 @@
 title Python Script to Executable
 echo Python Script to Executable by wsnek
 echo.
-echo Please make sure to have 'pyinstaller' installed: pip install pyinstaller
+echo Please make sure to have 'pyinstaller' installed, to get it use: pip install pyinstaller
+
 
 TIMEOUT /t 10 /nobreak
 
-pyinstaller -F --noconfirm --clean --hidden-import=numpy OCRTesseract.py 
+pyinstaller --onefile --distpath=./ --noconfirm --clean --workpath=./temp --name=OCRTesseract --log-level=WARN --hidden-import=numpy OCRTesseract.py 
 
 
 echo.
 echo pyinstaller compilation of program is now complete
 echo.
 echo Program can now be exited
+echo.
 PAUSE
